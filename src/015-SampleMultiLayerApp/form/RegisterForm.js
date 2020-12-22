f8c3d2b7fdb2bfb6ba971b4afb686af8e9ef7837
Oğuz Karan <oguzkaran@csystem.org>
2020-12-22 21:06:40 +0300

    ReactNative-Sep-2020

--- src/015-SampleMultiLayerApp/form/RegisterForm.js
+++ src/015-SampleMultiLayerApp/form/RegisterForm.js
@@ -1,15 +1,19 @@
 import React, {useState} from 'react';
-import { StyleSheet, Text, View, TouchableOpacity, TextInput} from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, TextInput, Switch} from 'react-native';
 
 import {mobileAppService} from '../global/global.js'
 import {UserInfo} from "../entity/UserInfo.js"
 import {ExceptionUtil} from "../util/ExceptionUtil.js"
+import {alertEmptyFieldsText, alertSaveFailText, alertNoSuchUserText} from '../resource/Resource.js'
+import {alertSaveSuccessText, alertUnexpectedStateText, alertInvalidOperationText} from '../resource/Resource.js'
+import {buttonSaveUserText, buttonDeleteAllUsersText, buttonListUsersText} from '../resource/Resource.js'
 
 const RegisterForm = props => {
     const [users, setUsers] = useState([])
     const [username, setUsername] = useState("oguzkaran")
     const [name, setName] = useState("Oğuz Karan")
     const [email, setEmail] = useState("oguzkaran@csystem.org")
+    const [deleteSwitchEnabled, setDeleteSwitchEnabled] = useState(false)
 
     const onListViewTouchableOpacityPress = ui => {
         const action = ui => {
@@ -50,13 +54,13 @@ const RegisterForm = props => {
     const onEmailChangeText = text => setEmail(text)
     const onSaveButtonPressed = () => {
         if (!validate()) {
-            alert("Alanlar boş geçilemez")
+            alert(alertEmptyFieldsText)
             return
         }
 
         const action = () => {
             const user = mobileAppService.saveUser(new UserInfo(0, username, name, email))
-            alert(user.id == 0 ? "Eklenemedi" : "Kayıt başarıyla eklendi")
+            alert(user.id == 0 ? alertSaveFailText : alertSaveSuccessText)
         }
 
         ExceptionUtil.subscribe(action, ex => ex.message)
@@ -66,13 +70,21 @@ const RegisterForm = props => {
         const action = () =>  {
             mobileAppService.deleteAllUsers()
             setUsers(mobileAppService.getAllUsers())
+            setDeleteSwitchEnabled(false)
         }
-        ExceptionUtil.subscribe(action, () => alert("Beklenmedik bir durum oluştu"))
+        ExceptionUtil.subscribe(action, () => alert(alertUnexpectedStateText))
     }
 
     const onListButtonPressed = () => {
-        const action = () => setUsers(mobileAppService.getAllUsers())
-        ExceptionUtil.subscribe(action, () => alert("Geçersiz işlem"))
+        const action = () => {
+            const allUsers = mobileAppService.getAllUsers()
+
+            if (allUsers.length != 0)
+                setUsers(allUsers)
+            else
+                alert(alertNoSuchUserText)
+        }
+        ExceptionUtil.subscribe(action, () => alert(alertInvalidOperationText))
     }
 
     return (
@@ -82,15 +94,18 @@ const RegisterForm = props => {
             <TextInput value={email} onChangeText={onEmailChangeText } placeholder="email"/>
             <TouchableOpacity onPress={onSaveButtonPressed}
                 style={{width: 200, height:25, backgroundColor: 'blue'}}>
-                <Text style={{textAlign: 'center'}}>Save</Text>
+                <Text style={{textAlign: 'center'}}>{buttonSaveUserText}</Text>
             </TouchableOpacity>
             <TouchableOpacity onPress={onDeleteAllUsersButtonPressed}
-                style={{width: 200, height:25, backgroundColor: 'red'}}>
-                <Text style={{textAlign: 'center'}}>Delete All Users</Text>
+                style={{width: 200, height:25, backgroundColor: 'red'}} disabled={!deleteSwitchEnabled}>
+                <Text style={{textAlign: 'center'}}>{buttonDeleteAllUsersText}</Text>
             </TouchableOpacity>
+            <Switch
+                onValueChange={() => setDeleteSwitchEnabled(!deleteSwitchEnabled)}
+                value={deleteSwitchEnabled} />
             <TouchableOpacity onPress={onListButtonPressed}
                 style={{width: 200, height:25, backgroundColor: 'gray'}}>
-                <Text style={{textAlign: 'center'}}>List</Text>
+                <Text style={{textAlign: 'center'}}>{buttonListUsersText}</Text>
             </TouchableOpacity>
             {
                 users.map((u, index) => setListView(u))
