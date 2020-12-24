da02ceee347d1e9fccf0cc1eec4bac13d00b035a
Oğuz Karan <oguzkaran@csystem.org>
2020-12-24 22:39:07 +0300

    ReactNative-Sep-2020

--- src/018-ReactNativeAlert/SampleComponent.js
+++ src/018-ReactNativeAlert/SampleComponent.js
@@ -2,45 +2,59 @@ import React, {useState} from 'react';
 import { StyleSheet, Text, View, TouchableOpacity, TextInput, Alert} from 'react-native';
 
 const SampleComponent = props => {
-    const [title, setTitle] = useState("")
-    const [message, setMessage] = useState("")
-    const [resultText, setResultText] = useState("")
+    const [name, setName] = useState("")
+    const [surname, setSurname] = useState("")
+    const [fullname, setFullname] = useState("")
+    const [isUpdated, setIsUpdated] = useState(false)
 
     const showAlert = () => {
-        Alert.alert(title, message,
+        Alert.alert("Uyarı", "Daha önce işlem yapmadınız. Emin misiniz?",
             [
                 {
                     text: "İptal",
-                    onPress: () => setResultText("İptal")
+                    onPress: () => {}
                 },
                 {
                     text: "Hayır",
-                    onPress: () => setResultText("Hayır")
+                    onPress: () => {}
                 },
                 {
                     text: "Evet",
-                    onPress: () => setResultText("Evet")
+                    onPress: () => clearViews()
                 }
             ]
         )
     }
 
+    const clearViews = () => {setName(""); setSurname(""); setFullname("")}
+
     const onOKButtonPressed = () => {
-        showAlert()
-        //...
+        setFullname(name + " " + surname)
+        setIsUpdated(false)
     }
 
-    const onTitleChangeText = text => setTitle(text)
-    const onMessageChangeText = text => setMessage(text)
+    const onClearButtonPressed = () => {
+        if (isUpdated)
+            showAlert()
+        else
+            clearViews()
+    }
+
+    const onNameChangeText = text => {setIsUpdated(true); setName(text)}
+    const onSurnameChangeText = text => {setIsUpdated(true); setSurname(text)}
 
     return (
         <View>
-            <TextInput value={title} onChangeText={onTitleChangeText}  placeholder="username"/>
-            <TextInput value={message} onChangeText={onMessageChangeText}  placeholder="username"/>
-            <Text>{resultText}</Text>
+            <TextInput value={name} onChangeText={onNameChangeText}  placeholder="name"/>
+            <TextInput value={surname} onChangeText={onSurnameChangeText}  placeholder="surname"/>
+            <Text>{fullname}</Text>
             <TouchableOpacity onPress={onOKButtonPressed}
                 style={{width: 200, height:25, backgroundColor: 'blue'}}>
                 <Text style={{textAlign: 'center'}}>OK</Text>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={onClearButtonPressed}
+                style={{width: 200, height:25, backgroundColor: 'blue'}}>
+                <Text style={{textAlign: 'center'}}>Clear</Text>
             </TouchableOpacity>
         </View>
     )
